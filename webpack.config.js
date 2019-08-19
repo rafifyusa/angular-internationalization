9a1059a5733bcca7de1c4fc7592b09c660d99721
Rafif Reyhandhia Yusa <Rafif_RY276@mitrais.com>
2019-08-19 17:13:44 +0800

    configured web-pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,59 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/main.ts',
-  resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src/app/'),
-    }
+  devServer: {
+    historyApiFallback: true,
   },
+  entry: "./src/main.ts",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: ['ts-loader', 'angular2-template-loader']
+        use: ["ts-loader", "angular2-template-loader"],
       },
       {
         test: /\.html$/,
-        use: 'html-loader'
+        use: "html-loader",
       },
       {
-        test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'less-loader', "scss-loader"]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
 
       // workaround for warning: System.import() is deprecated and will be removed soon. Use import() instead.
       {
+        parser: {system: true},
         test: /[\/\\]@angular[\/\\].+\.js$/,
-        parser: {system: true}
-      }
-    ]
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: "all",
+    },
   },
   plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new HtmlWebpackPlugin({template: "./src/index.html"}),
     new webpack.DefinePlugin({
       // global app config object
       config: JSON.stringify({
-        apiUrl: 'http://localhost:4000'
-      })
+        apiUrl: "http://localhost:4000",
+      }),
     }),
 
     // workaround for warning: Critical dependency: the request of a dependency is an expression
     new webpack.ContextReplacementPlugin(
       /\@angular(\\|\/)core(\\|\/)fesm5/,
-      path.resolve(__dirname, 'src')
-    )
+      path.resolve(__dirname, "src"),
+    ),
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src/app/"),
     },
-    runtimeChunk: true,
+    extensions: [".ts", ".js"],
   },
-  devServer: {
-    historyApiFallback: true,
-  };
-}
+};
